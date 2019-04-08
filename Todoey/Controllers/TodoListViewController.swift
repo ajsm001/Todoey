0d46e4e96491b5607924a2ba6c749be0d495a332
Gus <mcintyre_angus@hotmail.com>
2019-04-08 21:49:57 +0100

    Convert to MVC design pattern

--- Todoey/Controllers/TodoListViewController.swift
+++ Todoey/Controllers/TodoListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class TodoListViewController: UITableViewController {
     
-    var itemArray = ["First thing", "Second thing", "Third thing"]
+    var itemArray = [Item]()
     
     let defaults = UserDefaults.standard
     
@@ -18,10 +18,23 @@ class TodoListViewController: UITableViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        if let items = defaults.array(forKey: "TodoListArray") as? [String] {
-            itemArray = items
-        }
-            
+        let newItem = Item()
+        newItem.title = "First thing"
+        itemArray.append(newItem)
+        
+        let newItem2 = Item()
+        newItem2.title = "Second thing"
+        itemArray.append(newItem2)
+        
+        let newItem3 = Item()
+        newItem3.title = "Third thing"
+        itemArray.append(newItem3)
+        
+        
+//        if let items = defaults.array(forKey: "TodoListArray") as? [item] {
+//            itemArray = items
+//        }
+        
         }
         
         //MARK - TableView Datasource Methods
@@ -33,11 +46,27 @@ class TodoListViewController: UITableViewController {
     
         override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
+            
+            
             let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
         
-            cell.textLabel?.text = itemArray[indexPath.row]
-        
+            let item = itemArray[indexPath.row]
+            
+            cell.textLabel?.text = item.title
+            
+            //Ternary Operator
+            // value = condition ? valueIfTrue : valueIfFalse
+            
+            cell.accessoryType = item.done == true ? .checkmark : .none
+            
+//            if item.done == true {
+//                cell.accessoryType = .checkmark
+//            } else {
+//                cell.accessoryType = .none
+//            }
+            
             return cell
+           
         
         }
     
@@ -45,17 +74,19 @@ class TodoListViewController: UITableViewController {
     
         override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
             
-            // print(itemArray[indexPath.row])
+            //print(itemArray[indexPath.row].title)
+            
+            itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+//  The above line replaces the 5 lines below, using the 'not' operator to reverse the condition
+//            if itemArray[indexPath.row].done == false {
+//                itemArray[indexPath.row].done = true
+//            } else {
+//                itemArray[indexPath.row].done = false
+//            }
+            
+            tableView.reloadData()
             
             tableView.deselectRow(at: indexPath, animated: true)
-            
-            if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
-                tableView.cellForRow(at: indexPath)?.accessoryType = .none
-            } else {
-                tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-            }
-            
-            
     
         }
     
@@ -71,7 +102,9 @@ class TodoListViewController: UITableViewController {
             let alertAction = UIAlertAction(title: "Add Item", style: .default) { (action) in
                 //what will happen once the user clicks the Add Item button on our UIAlert
                 
-                self.itemArray.append(textField.text!)
+                let newItem = Item()
+                newItem.title = textField.text!
+                self.itemArray.append(newItem)
                 
                 self.defaults.set(self.itemArray, forKey: "TodoListArray")
                 
---
+++ Todoey/Data Model/item.swift
@@ -0,0 +1,14 @@
+//
+//  item.swift
+//  Todoey
+//
+//  Created by Angus McIntyre on 08/04/2019.
+//  Copyright © 2019 Angus McIntyre. All rights reserved.
+//
+
+import Foundation
+
+class Item {
+    var title : String = ""
+    var done : Bool = false
+}
